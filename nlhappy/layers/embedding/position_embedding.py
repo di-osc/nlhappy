a07b2d37d9700adc593c3e7401ff1d38749ed52b
good-as-water <790990241@qq.com>
2022-04-29 11:33:27 +0800

    完成位置编码

--- nlhappy/layers/embedding/position_embedding.py
+++ nlhappy/layers/embedding/position_embedding.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import Module
+import torch.nn as nn
 import math
 
 
@@ -22,35 +22,82 @@ def get_sinusoid_encoding_table(n_position, d_hid, padding_idx=None):
     position_ids[:, 1::2] = torch.cos(position_ids[:, 1::2])
     return position_ids
 
+class RelativePositionEmbedding(nn.Module):
+    def __init__(self,
+                 qlen,
+                 klen,
+                 embedding_size,
+                 max_relative_position=127) -> None:
+        super(RelativePositionEmbedding, self).__init__()
+        vocab_size = max_relative_position * 2 + 1
+        distance_mat = torch.arange(klen)[None, :] - torch.arange(qlen)[:, None]  # 列数-行数, [query_len, key_len]
+        distance_mat_clipped = torch.clamp(distance_mat, -max_relative_position, max_relative_position)
+        final_mat = distance_mat_clipped + max_relative_position
 
-class SinusoidalPositionEmbedding(Module):
+        # sinusoid_encoding编码的位置矩阵
+        embeddings_table = get_sinusoid_encoding_table(vocab_size, embedding_size)
+
+        # 实现方式1
+        # flat_relative_positions_matrix = final_mat.view(-1)
+        # one_hot_relative_positions_matrix = torch.nn.functional.one_hot(flat_relative_positions_matrix, num_classes=vocab_size).float()
+        # position_embeddings = torch.matmul(one_hot_relative_positions_matrix, embeddings_table)
+        # my_shape = list(final_mat.size())
+        # my_shape.append(embedding_size)
+        # position_embeddings = position_embeddings.view(my_shape)
+
+        # 实现方式2
+        # position_embeddings = torch.take_along_dim(embeddings_table, final_mat.flatten().unsqueeze(1), dim=0)
+        # position_embeddings = position_embeddings.reshape(*final_mat.shape, embeddings_table.shape[-1])  # [seq_len, seq_len, hdsz]
+        # self.register_buffer('position_embeddings', position_embeddings)
+        
+        # 实现方式3
+        position_embeddings = nn.Embedding.from_pretrained(embeddings_table, freeze=True)(final_mat)
+        self.register_buffer('position_embeddings', position_embeddings)
+
+    def forward(self, qlen, klen):
+        return self.position_embeddings[:qlen, :klen, :]
+    
+    
+class SinusoidalPositionEmbedding(nn.Module):
     """定义Sin-Cos位置Embedding
     """
-    def __init__(
-        self,
-        output_size,
-        merge_mode='add',
-        custom_position_ids=False
-    ):
+    def __init__(self, max_position, embedding_size):
         super(SinusoidalPositionEmbedding, self).__init__()
-        self.output_size = output_size
-        self.merge_mode = merge_mode
-        self.custom_position_ids = custom_position_ids
+        position_embeddings = nn.Embedding.from_pretrained(get_sinusoid_encoding_table(max_position, embedding_size), freeze=True)
+        self.register_buffer('position_embeddings', position_embeddings)
 
-    def forward(self, inputs):
-        input_shape = inputs.shape
-        _, seq_len = input_shape[0], input_shape[1]
-        position_ids = torch.arange(seq_len).type(torch.float)[None]
-        indices = torch.arange(self.output_size // 2).type(torch.float)
-        indices = torch.pow(10000.0, -2 * indices / self.output_size)
-        embeddings = torch.einsum('bn,d->bnd', position_ids, indices)
-        embeddings = torch.stack([torch.sin(embeddings), torch.cos(embeddings)], dim=-1)
-        embeddings = torch.reshape(embeddings, (-1, seq_len, self.output_size))
+    def forward(self, position_ids):
+        return self.position_embeddings(position_ids)
 
-        if self.merge_mode == 'add':
-            return inputs + embeddings.to(inputs.device)
-        elif self.merge_mode == 'mul':
-            return inputs * (embeddings + 1.0).to(inputs.device)
-        elif self.merge_mode == 'zero':
-            return embeddings.to(inputs.device)
+
+# class SinusoidalPositionEmbedding(nn.Module):
+#     """定义Sin-Cos位置Embedding
+#     """
+#     def __init__(
+#         self,
+#         output_size,
+#         merge_mode='add',
+#         custom_position_ids=False
+#     ):
+#         super(SinusoidalPositionEmbedding, self).__init__()
+#         self.output_size = output_size
+#         self.merge_mode = merge_mode
+#         self.custom_position_ids = custom_position_ids
+
+#     def forward(self, inputs):
+#         input_shape = inputs.shape
+#         _, seq_len = input_shape[0], input_shape[1]
+#         position_ids = torch.arange(seq_len).type(torch.float)[None]
+#         indices = torch.arange(self.output_size // 2).type(torch.float)
+#         indices = torch.pow(10000.0, -2 * indices / self.output_size)
+#         embeddings = torch.einsum('bn,d->bnd', position_ids, indices)
+#         embeddings = torch.stack([torch.sin(embeddings), torch.cos(embeddings)], dim=-1)
+#         embeddings = torch.reshape(embeddings, (-1, seq_len, self.output_size))
+
+#         if self.merge_mode == 'add':
+#             return inputs + embeddings.to(inputs.device)
+#         elif self.merge_mode == 'mul':
+#             return inputs * (embeddings + 1.0).to(inputs.device)
+#         elif self.merge_mode == 'zero':
+#             return embeddings.to(inputs.device)
 
