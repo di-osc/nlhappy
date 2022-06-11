01f361931c3a2509f00aae0f9bc11e057f8ba201
善若水 <790990241@qq.om>
2022-06-11 23:12:00 +0000

    增加导入

--- nlhappy/components/__init__.py
+++ nlhappy/components/__init__.py
@@ -3,3 +3,4 @@ from .token_classifier import make_tokencat
 from .text_classifier import make_text_classification
 from .sentence_segmenter import make_sentence_segmenter
 from .triple_extractor import make_triple_extractor
+from .event_extractor import make_event_extractor
