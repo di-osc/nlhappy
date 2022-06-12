e57f0b22019759be3de201e9bc81edeb170e8dc9
善若水 <790990241@qq.om>
2022-06-12 03:29:44 +0000

    添加nlp

--- nlhappy/components/__init__.py
+++ nlhappy/components/__init__.py
@@ -4,3 +4,11 @@ from .text_classifier import make_text_classification
 from .sentence_segmenter import make_sentence_segmenter
 from .triple_extractor import make_triple_extractor
 from .event_extractor import make_event_extractor
+
+
+def get_chinese_nlp():
+    import spacy
+    from .tokenizer import CharTokenizer
+    nlp = spacy.blank('zh')
+    nlp.tokenizer = CharTokenizer(nlp.vocab)
+    return nlp
