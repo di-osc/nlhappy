9324e15af7859270733efdb4f2a1505f45e0eb22
善若水 <790990241@qq.om>
2022-06-16 01:41:21 +0000

    feat[metrics.triple]: new class Triple for store spoes

--- nlhappy/metrics/triple.py
+++ nlhappy/metrics/triple.py
@@ -1,7 +1,43 @@
 from torchmetrics import Metric
 import torch
 from typing import Tuple, Set, List
-from ..utils.data import Triple
+
+class Triple(tuple):
+    """存放三元组的五元形式(sub_start, sub_end, predicate, obj_start, obj_end), 这样改写方便求交集"""
+    
+    def __init__(self, triple: Tuple[int, str]):
+        super().__init__()
+        self.triple = triple
+        
+    def __hash__(self):
+        return self.triple.__hash__()
+
+    def __eq__(self, triple):
+        return self.triple == triple.triple
+
+    def __repr__(self) -> str:
+        return str(self.triple)
+
+    def __len__(self) -> int:
+        return len(self.triple)
+
+    def __getitem__(self, index):
+        return self.triple[index]
+
+    @property
+    def subject(self):
+        """返回subject的下标, 左闭右开"""
+        return (self.triple[0], self.triple[1])
+       
+    @property
+    def predicate(self):
+        return self.triple[2]
+        
+    
+    @property
+    def object(self):
+        """返回object的下标, 左闭右开"""
+        return (self.triple[3], self.triple[4])
 
 class TripleF1(Metric):
     """三元组抽取的F1
