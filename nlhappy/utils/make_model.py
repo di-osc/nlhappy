34f9b4a8643e79d826bfc6bed16ab47b41d78942
善若水 <790990241@qq.om>
2022-06-16 14:23:11 +0000

    feat[utils.make_model]: new function for get tokenizer from config and vocab

--- nlhappy/utils/make_model.py
+++ nlhappy/utils/make_model.py
@@ -1,5 +1,13 @@
+from typing import Dict
+import os
+from transformers import AutoTokenizer
 
-
-
-def get_hf_tokenizer(config, vocab):
-    pass 
+def get_hf_tokenizer(config: Dict, vocab: Dict):
+    with open('./vocab.txt', 'w') as f:
+        for k in vocab.keys():
+            f.writelines(k + '\n')
+    config.to_json_file('./config.json')
+    tokenizer = AutoTokenizer.from_pretrained('./')
+    os.remove('./vocab.txt')
+    os.remove('./config.json')
+    return tokenizer
