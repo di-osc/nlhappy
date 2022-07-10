bf4c68297e67270f46af52a239fa3dbc8b6d94b8
善若水 <790990241@qq.om>
2022-07-10 05:49:11 +0000

    change import

--- nlhappy/components/__init__.py
+++ nlhappy/components/__init__.py
@@ -2,7 +2,7 @@ from .span_classifier import make_spancat
 from .token_classifier import make_tokencat
 from .text_classifier import make_text_classification
 from .sentence_segmenter import make_sentence_segmenter
-from .triple_extractor import make_triple_extractor
+from .relation_extractor import make_relation_extractor
 from .event_extractor import make_event_extractor
 from .information_extractor import make_ie
 from .entity_normalizer import make_entity_normalizer
