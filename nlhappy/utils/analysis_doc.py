e137db21154a8c03101eac6578af8e77408e9e68
善若水 <790990241@qq.om>
2022-07-10 02:43:44 +0000

    New function for analysis relations

--- nlhappy/utils/analysis_doc.py
+++ nlhappy/utils/analysis_doc.py
@@ -16,6 +16,8 @@ def is_badcase(pred: Doc, true: Doc):
         return true.spans['all'] != pred.spans['all']
     elif len(true._.triples) >0:
         return true._.triples != pred._.triples
+    elif len(true._.relations) >0:
+        return true._.relations != pred._.relations
     else: print("未能检查到标注数据")
     
 def is_triple_badcase(pred: Doc, true: Doc):
@@ -85,3 +87,57 @@ def analysis_triple_badcase(preds: List[Doc], docs:List[Doc], return_prf:bool=Fa
         return_prf (bool, optional): 是否返回prf字典. Defaults to False.
     """
     pass
+
+def analysis_relation_badcase(examples: List[Example] , return_prf:bool=True):
+    """错误分析,并且返回细粒度的指标
+
+    Args:
+        examples (List[Example]): 一个列表,里面为Example
+        return_prf (bool, optional): 是否返回prf分数. Defaults to True.
+
+    Returns:
+        List[Examples], Dict : badcase, prf分数
+    """
+    all_preds = 0
+    all_corrects = 0
+    all_trues = 0
+    per_types = {rel.label : {'preds':0, 'trues':0, 'corrects':0} for e in examples for rel in e.y._.relations}
+    badcases = []
+    for e in examples:
+        corrects = [ rel for rel in e.x._.relations if rel in e.y._.relations]
+        if len(corrects) != len(e.y._.relations):
+            badcases.append(e)
+        if return_prf:
+            all_trues += len(e.y._.relations)
+            all_preds += len(e.x._.relations)
+            all_corrects += len(corrects)
+            for rel in e.y._.relations:
+                per_types[rel.label]['trues'] +=1
+            for rel in e.x._.relations:
+                per_types[rel.label]['preds'] +=1
+                if rel in e.y._.relations:
+                    per_types[rel.label]['corrects'] +=1
+    if return_prf:
+        rel_p = all_corrects/(all_preds + 1e-8)
+        rel_r = all_corrects/(all_trues + 1e-8)
+        rel_f = 2*rel_p*rel_r/(rel_p + rel_r + 1e-8)
+        rel_per_types = {l:{} for l in per_types}
+        for l, scores in per_types.items():
+            p = scores['corrects'] / (scores['preds'] + 1e-8)
+            r = scores['corrects'] / (scores['trues'] + 1e-8)
+            f = 2 * p * r / (p + r + 1e-8)
+            rel_per_types[l]['p'] = p
+            rel_per_types[l]['r'] = r
+            rel_per_types[l]['f'] = f
+            
+        scores = {'rel_micro_p':rel_p, 
+                'rel_micro_r': rel_r,
+                'rel_micro_f': rel_f,
+                'rel_per_types':rel_per_types}
+        return badcases, scores
+    else:
+        return badcases
+            
+        
+        
+    
