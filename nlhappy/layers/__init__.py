5d1feadbfccaf7f3674b08c01f3689c298851cf0
善若水 <790990241@qq.om>
2022-07-13 03:27:16 +0000

    New layer Mdrop

--- nlhappy/layers/__init__.py
+++ nlhappy/layers/__init__.py
@@ -5,6 +5,7 @@ from .embedding import SinusoidalPositionEmbedding
 from .activation import GELU, SWISH, GELU_Approximate
 from .transformer import Bert, BertEmbeddings, BertAttention, BertAddNorm, BertEncoder, BertPooler
 from .word2vec import SkipGram
+from .dropout import MultiDropout
 
 
 
