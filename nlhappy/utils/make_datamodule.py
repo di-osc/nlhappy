4675a3811f438c819649b4bb34d00a11372898df
善若水 <790990241@qq.om>
2022-07-14 07:14:11 +0000

    Refacotr align funcs

--- nlhappy/utils/make_datamodule.py
+++ nlhappy/utils/make_datamodule.py
@@ -4,6 +4,8 @@ import zipfile
 from .utils import get_logger
 from typing import List
 
+log = get_logger()
+
 def char_idx_to_token(char_idx, offset_mapping):
     """
     将char级别的idx 转换为token级别的idx
@@ -135,31 +137,84 @@ def prepare_data_from_oss(dataset: str,
         log.info('all data are ready')
         
         
-def align_char_span(char_span_offset: List, token_offset_mapping, special_offset=(0,0)) -> List:
+def align_char_span(char_span_offset: tuple, 
+                    token_offset_mapping, 
+                    special_offset=(0,0),
+                    add_special: bool=False) -> tuple:
     '''对齐字符级别的span标签与bert的子词切分的标签
     参数:
-    - char_span_offset: 字符级别的文本片段下标,例如(0,1)
+    - char_span_offsets: 字符级别的文本片段下标,例如(0,1)
     - token_offset_mapping: 词符与字符下标映射例如[(0,1), (1,2), (2,3) ...]
     输出
     token_span_offset: 词符级别的文本片段下标,例如(0,2)
     '''
-    token_span_offset = []
+    token_span_offset = ()
+    bias = 0
     for i, offset in enumerate(token_offset_mapping):
+        if offset == special_offset:
+            bias += 1
+            continue
         if offset != special_offset:
             if offset[0] == char_span_offset[0]:
                 start = i
                 if offset[1] == char_span_offset[1]:
                     end = i+1
                     break
-                else: next
-            elif offset[1] == char_span_offset[1]:
+                else: 
+                    continue
+            if offset[1] == char_span_offset[1]:
                 end = i+1
                 break
     try:
-        if start and end:
-            token_span_offset = [start, end]
-    except Exception:
-        pass
+        if add_special:
+            token_span_offset = (start, end)
+        else:
+            token_span_offset = (start-bias, end-bias)     
+    except:
+        log.warning('align offset failed')
     return token_span_offset
+
+def align_char_span_text_b(char_span_offset: tuple, 
+                            token_offset_mapping, 
+                            special_offset=(0,0),
+                            add_pre: bool=True) -> tuple:
+    """对齐文本b的token下标, 适用于bert text pair形式
+
+    Args:
+        char_span_offset (tuple): 文本中的char级别的offset
+        token_offset_mapping (_type_): 映射字典
+        special_offset (tuple, optional): 特殊token offset. Defaults to (0,0).
+        add_pre (bool, optional): 是否加上之前的token. Defaults to True.
+
+    Returns:
+        tuple: token级别的offset
+    """
+    token_span_offset = ()
+    bias = 0
+    for i, offset in enumerate(token_offset_mapping):
+        if offset == special_offset and i ==0:
+            continue
+        if offset == special_offset:
+            bias = i+1
+        if offset != special_offset and bias>0:
+            if offset[0] == char_span_offset[0]:
+                start = i
+                if offset[1] == char_span_offset[1]:
+                    end = i+1
+                    break
+                else: 
+                    continue
+            if offset[1] == char_span_offset[1]:
+                end = i+1
+                break
+    try:
+        if add_pre:
+            token_span_offset = (start, end)
+        else:
+            token_span_offset = (start-bias, end-bias)     
+    except:
+        log.warning(f'align {char_span_offset} failed')
+    return token_span_offset
+    
 
 
