b1973c6d9e9a394f4ec6963b254f46c46912fc26
善若水 <790990241@qq.om>
2022-07-14 07:13:06 +0000

    New model global relation

--- nlhappy/models/__init__.py
+++ nlhappy/models/__init__.py
@@ -6,3 +6,4 @@ from .text_multi_classification import BertTextMultiClassification
 from .text_pair_classification import BERTBiEncoder, BERTCrossEncoder
 from .text_pair_regression import SentenceBERT
 from .prompt_span_extraction import BERTGlobalSpan
+from .prompt_relation_extraction import GlobalRelation
