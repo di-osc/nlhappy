e5befd20efd4b6b89183081c59430ee6f2af7074
善若水 <790990241@qq.om>
2022-07-15 08:12:32 +0000

    Refactor text classifier component

--- nlhappy/components/text_classifier.py
+++ nlhappy/components/text_classifier.py
@@ -1,42 +1,71 @@
 from spacy.lang.zh import Chinese
 from ..models import BertTextClassification
 import torch
-import shutil
 from spacy.tokens import Doc
+import os
+import pickle
+import logging
 
-models = {'bert_text_classification': BertTextClassification}
+log = logging.getLogger()
+
 
 Doc.set_extension('labels', default=[])
 
-class TextClassification:
+class BertTextClassifier:
     '''spacy 文本分类管道'''
-    def __init__(self, nlp, name:str, model_name: str, ckpt: str, device: str):
+    def __init__(self, 
+                 nlp, 
+                 name:str, 
+                 device: str,
+                 threshold: float):
         super().__init__()
         self.nlp = nlp
         self.pipe_name = name
-        self.ckpt = ckpt
         self.device = torch.device(device)
-        self.model_class = models[model_name]
-        self.model = models[model_name].load_from_checkpoint(ckpt)
-
+        self.threshold = threshold
         
     def __call__(self, doc: Doc) -> Doc:
         with torch.no_grad():
-            doc.cats = self.model.predict(doc.text, device=self.device)
+            scores = self.model.predict(doc.text, device=self.device)
+            if scores[0][1] >= self.threshold:
+                doc._.label = scores[0][0]
+            doc.cats = dict(scores)
         return doc
+    
+    def init_model(self, model_or_path):
+        if isinstance(model_or_path, BertTextClassification):
+            self.model = model_or_path
+            self.model.to(self.device)
+            self.model.freeze()
+            
+        else:
+            self.model= BertTextClassification.load_from_checkpoint(model_or_path)
+            self.model.to(self.device)
+            self.model.freeze()
 
-    def to_disk(self, path: str, exclude):
-        shutil.copy(self.ckpt, path)
-        self.nlp.config['components'][self.pipe_name]['ckpt'] = path
-
-    def from_disk(self, path: str, exclude):
-        self.model = self.model_class.load_from_checkpoint(path)
+    def to_disk(self, path:str, exclude):
+        if not os.path.exists(path):
+            os.mkdir(path=path)
+        model = 'tc.pkl'
+        model_path = os.path.join(path, model)
+        with open(model_path, 'wb') as f:
+            pickle.dump(self.model, f)
+        
+    def from_disk(self, path:str, exclude):
+        model_path = os.path.join(path, 'tc.pkl')
+        with open(model_path, 'rb') as f:
+            self.model = pickle.load(f)
+        try:
+            self.model.to(self.device)
+        except:
+            log.info(f' to device {self.device} failed')
         self.model.freeze()
 
-
-@Chinese.factory('text_classifier', default_config={'model_name':'bert_text_classification'})
-def make_text_classification(nlp, name:str, model_name: str, ckpt: str, device: str):
-    return TextClassification(nlp, name, model_name, ckpt, device)
+default_config = {'model': 'bert_tc', 'device':'cpu', 'threshold':0.8}
+@Chinese.factory('text_classifier', default_config=default_config)
+def make_text_classification(nlp, name:str, device:str, model: str, threshold: float):
+    if model == 'bert_tc':
+        return BertTextClassifier(nlp, name=name, device=device, threshold=threshold)
 
 
     
