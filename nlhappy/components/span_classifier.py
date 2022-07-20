10be86b8b1d230712cc979da95f3f2c7e6d9c05d
善若水 <790990241@qq.om>
2022-07-20 13:23:38 +0000

    Refactor span_classifier component

--- nlhappy/components/span_classifier.py
+++ nlhappy/components/span_classifier.py
@@ -1,35 +1,26 @@
-import shutil
-from ..models import BertGlobalPointer
+from ..models import GlobalPointer
 from spacy.lang.zh import Chinese
-from ..models import BertGlobalPointer
 import torch
 from spacy.tokens import Doc
-from thinc.api import Config
 import os
 from spacy.util import filter_spans
+import pickle
+from nlhappy.utils.utils import get_logger
 
-models = {'bert_global_pointer': BertGlobalPointer}
+
+log = get_logger()
 
 class SpanClassification:
     '''句子级别span分类spacy pipeline
     - ckpt: 模型保存路径
     '''
-    def __init__(self, nlp, name:str, model:str, ckpt:str, device:str, sentence_level:bool, threshold:float, set_ents: bool):
+    def __init__(self, nlp, name:str, device:str, sentence_level:bool, threshold:float, set_ents: bool):
         self.nlp = nlp
         self.pipe_name = name
-        self.ckpt = ckpt
         self.set_ents = set_ents
         self.sentence_level = sentence_level
         self.device = torch.device(device)
         self.threshold = threshold
-        self.model_name = model
-        try:
-            self.model = models[self.model_name].load_from_checkpoint(self.ckpt)
-            self.model.to(self.device)
-            self.model.freeze()
-        except Exception:
-            pass
-        
         
         
     def __call__(self, doc: Doc) -> Doc:
@@ -53,25 +44,38 @@ class SpanClassification:
 
     def to_disk(self, path:str, exclude):
         # 复制原来模型参数到新的路径
-        shutil.copy(self.ckpt, path)
-        # 重写NLP配置文件config.cfg 改变pipeline的ckpt路径
-        # nlp_path = str(path).split(self.pipe_name)[0]
-        # config_path = os.path.join(nlp_path, 'config.cfg')
-        # config = Config().from_disk(config_path)
-        # config['components'][self.pipe_name]['ckpt'] = str(path)
-        # config.to_disk(config_path)
+        if not os.path.exists(path):
+            os.mkdir(path=path)
+        model = 'sc.pkl'
+        model_path = os.path.join(path, model)
+        with open(model_path, 'wb') as f:
+            pickle.dump(self.model, f)
 
     def from_disk(self, path:str, exclude):
-        nlp_path = str(path).split(self.pipe_name)[0]
-        config_path = os.path.join(nlp_path, 'config.cfg')
-        config = Config().from_disk(config_path)
-        ckpt_name = config['components'][self.pipe_name]['ckpt'].split('/')[-1]
-        ckpt_path = os.path.join(path, ckpt_name)
-        self.model = models[self.model_name].load_from_checkpoint(ckpt_path)
+        model_path = os.path.join(path, 'sc.pkl')
+        with open(model_path, 'rb') as f:
+            self.model = pickle.load(f)
         self.model.freeze()
+        try:
+            self.model.to(self.device)
+        except:
+            log.info(f' to device {self.device} failed')
+            
+    def init_model(self, model_or_path):
+        if isinstance(model_or_path, GlobalPointer):
+            self.model = model_or_path
+            self.model.freeze()
+            self.model.to(self.device)
+            
+        else:
+            self.model= GlobalPointer.load_from_checkpoint(model_or_path)
+            self.model.freeze()
+            self.model.to(self.device)
+            
+default_config={'device':'cpu', 'sentence_level':False, 'threshold':0.5,'set_ents':True}
 
-@Chinese.factory('span_classifier',assigns=['doc.spans'],default_config={'model':'bert_global_pointer', 'device':'cpu', 'sentence_level':False, 'threshold':0.5,'set_ents':False})
-def make_spancat(nlp, name:str, model:str, ckpt:str, device:str, sentence_level:bool, threshold:float, set_ents: bool):
+@Chinese.factory('span_classifier',assigns=['doc.spans'],default_config=default_config)
+def make_spancat(nlp, name:str, device:str, sentence_level:bool, threshold:float, set_ents: bool):
     """句子级别的文本片段分类"""
-    return SpanClassification(nlp=nlp, name=name, model=model, ckpt=ckpt, device=device, sentence_level=sentence_level, threshold=threshold, set_ents=set_ents)
+    return SpanClassification(nlp=nlp, name=name, device=device, sentence_level=sentence_level, threshold=threshold, set_ents=set_ents)
 
