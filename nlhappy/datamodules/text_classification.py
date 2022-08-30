a8eb98c24f419f324a2c2e09adef12f7b564b490
善若水 <790990241@qq.om>
2022-08-30 07:58:07 +0000

    Refactor text classification datamodule

--- nlhappy/datamodules/text_classification.py
+++ nlhappy/datamodules/text_classification.py
@@ -1,31 +1,22 @@
-import pytorch_lightning as pl
+from functools import lru_cache
 from typing import Tuple, List, Dict
-from transformers import AutoTokenizer, AutoConfig
-from ..utils.make_datamodule import prepare_data_from_remote
-from torch.utils.data import DataLoader
-from datasets import load_from_disk
-import torch
-import os
+from ..utils.make_datamodule import PLMBaseDataModule
 from ..utils import utils
+import torch
 
 log = utils.get_logger(__name__)
 
-example = '''
-        单标签:
-        {'label': '新闻', 'text': '怎么给这个图片添加超级链接呢？'}
-        多标签:
-        {'labels': ['新闻', '财经'], 'text': '怎么给这个图片添加超级链接呢？'}
-        '''
 
-class TextClassificationDataModule(pl.LightningDataModule):
+class TextClassificationDataModule(PLMBaseDataModule):
     '''
     文本分类
     '''
     def __init__(self,
                 dataset: str,
                 plm: str,
-                max_length: int ,
                 batch_size: int ,
+                transform: str,
+                auto_length: str,
                 num_workers: int =0,
                 pin_memory: bool =False,
                 plm_dir: str ='./plms/',
@@ -35,7 +26,6 @@ class TextClassificationDataModule(pl.LightningDataModule):
         Args:
             dataset (str): 数据集名称
             plm (str): 预训练模型名称
-            max_length (int): 文本最大长度
             batch_size (int): 批次大小
             num_workers (int, optional): _description_. Defaults to 0.
             pin_memory (bool, optional): _description_. Defaults to False.
@@ -43,90 +33,47 @@ class TextClassificationDataModule(pl.LightningDataModule):
             dataset_dir (str, optional): _description_. Defaults to './datasets/'.
         """
         
-        super().__init__()
-
-        # 这一行代码为了保存传入的参数可以当做self.hparams的属性
-        self.save_hyperparameters(logger=False)
+        super().__init__()        
+        self.transforms = {'simple': self.simple_transform}
+        assert self.hparams.transform in self.transforms.keys(), f'availabel models for relation extraction: {self.transforms.keys()}'
         
-        
-        
-        
-
-
-    def prepare_data(self):
-        '''
-        下载数据集.这个方法只会在一个GPU上执行一次.
-        '''
-        prepare_data_from_oss(dataset=self.hparams.dataset,
-                              plm=self.hparams.plm,
-                              dataset_dir=self.hparams.dataset_dir,
-                              plm_dir=self.hparams.plm_dir)
-        
-
-    def transform(self, examples) -> Dict:
-        batch_text = examples['text']
-        batch_label = examples['label']
-        batch = {'inputs':[], 'label_ids':[]}
-        for i, text in enumerate(batch_text):
-            inputs = self.tokenizer(
-                text, 
-                padding='max_length',  
-                max_length=self.hparams.max_length+2,
-                truncation=True)
-            inputs = dict(zip(inputs.keys(), map(torch.tensor, inputs.values())))
-            batch['inputs'].append(inputs)
-            batch['label_ids'].append(self.hparams['label2id'][batch_label[i]])
-        return batch
-        
-
         
     def setup(self, stage: str) -> None:
-        dataset_path = os.path.join(self.hparams.dataset_dir, self.hparams.dataset)
-        self.dataset = load_from_disk(dataset_path)
-        set_labels = set(self.dataset['train']['label'])
-        label2id = {label: i for i, label in enumerate(set_labels)}
-        id2label = {i: label for label, i in label2id.items()}
-        self.hparams['label2id'] = label2id
-        self.hparams['id2label'] = id2label
-        plm_path = os.path.join(self.hparams.plm_dir, self.hparams.plm)
-        self.tokenizer = AutoTokenizer.from_pretrained(plm_path)
-        # self.hparams['vocab'] = dict(self.tokenizer.vocab)
-        self.hparams['vocab'] = dict(sorted(self.tokenizer.vocab.items(), key=lambda x: x[1]))
-        trf_config = AutoConfig.from_pretrained(plm_path)
-        self.hparams['trf_config'] = trf_config
-        self.dataset.set_transform(transform=self.transform)
-            
+        self.hparams.max_length = self.get_max_length()
+        self.hparams.label2id = self.label2id
+        self.hparams.id2label = {i:l for l, i in self.label2id.items()}
+        self.dataset.set_transform(transform=self.simple_transform)
 
-    def train_dataloader(self):
-        '''
-        返回训练集的DataLoader.
-        '''
-        return DataLoader(
-            dataset= self.dataset['train'], 
-            batch_size=self.hparams.batch_size, 
-            num_workers=self.hparams.num_workers, 
-            pin_memory=self.hparams.pin_memory,
-            shuffle=True)
+    
+    def simple_transform(self, examples) -> Dict:
+        batch_text = examples['text']
+        batch_inputs = {'input_ids':[], 'token_type_ids':[], 'attention_mask':[]}
+        batch_label_ids = []
+        max_length = self.hparams.max_length
+        for i, text in enumerate(batch_text):
+            inputs = self.tokenizer(text, 
+                                    padding='max_length',  
+                                    max_length=max_length,
+                                    truncation=True)
+            batch_inputs['input_ids'].append(inputs['input_ids'])
+            batch_inputs['token_type_ids'].append(inputs['token_type_ids'])
+            batch_inputs['attention_mask'].append(inputs['attention_mask'])
+            batch_label_ids.append(self.hparams['label2id'][examples['label'][i]])
+        batch_inputs['label_ids'] = batch_label_ids
+        batch = dict(zip(batch_inputs.keys(), map(torch.tensor, batch_inputs.values())))
         
-    def val_dataloader(self):
-        '''
-        返回验证集的DataLoader.
-        '''
-        return DataLoader(
-            dataset=self.dataset['validation'], 
-            batch_size=self.hparams.batch_size, 
-            num_workers=self.hparams.num_workers, 
-            pin_memory=self.hparams.pin_memory,
-            shuffle=False)
+        return batch
 
-    def test_dataloader(self):
-        '''
-        返回验证集的DataLoader.
-        '''
-        return DataLoader(
-            dataset=self.dataset['test'], 
-            batch_size=self.hparams.batch_size, 
-            num_workers=self.hparams.num_workers, 
-            pin_memory=self.hparams.pin_memory,
-            shuffle=False)
+
+    @property
+    @lru_cache()
+    def label2id(self):
+        labels = set(self.train_df.label.values)
+        label2id = {l : i for i,l in enumerate(labels)}
+        return label2id
+
+        
+    @staticmethod
+    def show_one_sample(self):
+        return {'label': '新闻', 'text': '怎么给这个图片添加超级链接呢？'}
         
