621ff396e8c63ed89ccbc6bd66e8eb93f6c2b39b
善若水 <790990241@qq.om>
2022-10-09 01:51:43 +0000

    Add biaffine

--- nlhappy/layers/classifier/biaffine.py
+++ nlhappy/layers/classifier/biaffine.py
@@ -33,4 +33,65 @@ class Biaffine(nn.Module):
         # remove dim 1 if output_size == 1
         s = s.permute(0, 2, 3, 1)
 
-        return s    
+        return s  
+    
+    
+class BiaffineClassifier(nn.Module):
+    def __init__(self, 
+                 input_size: int, 
+                 output_size: int, 
+                 hidden_size: int,
+                 tril_mask: bool = True,
+                 bias_x: bool = True, 
+                 bias_y: bool = True):
+        super().__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+        self.bias_x = bias_x
+        self.bias_y = bias_y
+        self.tril_mask = tril_mask
+        weight = torch.zeros((output_size, hidden_size + int(bias_x), hidden_size + int(bias_y)))
+        nn.init.xavier_normal_(weight)                                          #将权重变为正态分布
+        self.weight = nn.Parameter(weight, requires_grad=True)
+        self.start_repr = nn.Sequential(nn.Linear(input_size, hidden_size), nn.ReLU())
+        self.end_repr = nn.Sequential(nn.Linear(input_size, hidden_size), nn.ReLU())
+
+    def extra_repr(self):
+        s = f"input_size={self.input_size}, output_size={self.output_size}"
+        if self.bias_x:
+            s += f", bias_x={self.bias_x}"
+        if self.bias_y:
+            s += f", bias_y={self.bias_y}"
+        return s
+
+    def forward(self, x, mask=None):
+        """将序列的表示(batch,seq,hidden),转换为token pair的表示
+
+        Args:
+            x : [batch_size, seq_len, hidden_size]
+            mask : [batch_size, seq_len]
+
+        Returns:
+            logits: [batch_size, output_size, seq_len, seq_len]
+        """
+        start_logits = self.start_repr(x)
+        end_logits = self.end_repr(x)
+        if self.bias_x:
+            start_logits = torch.cat((start_logits, torch.ones_like(start_logits[..., :1])), -1) #加偏执项 
+        if self.bias_y:
+            end_logits = torch.cat((end_logits, torch.ones_like(end_logits[..., :1])), -1)
+        # [batch_size, output_size, seq_len, seq_len]
+        span_logits = torch.einsum('bxi,oij,byj->boxy', start_logits, self.weight, end_logits)  #矩阵点积
+        
+        if mask is not None:
+            batch_size = x.size()[0]
+            seq_len = x.size()[1]
+            pad_mask = mask.unsqueeze(1).unsqueeze(1).expand(batch_size, self.output_size, seq_len, seq_len)
+            span_logits = span_logits*pad_mask - (1-pad_mask) * 1e12
+        
+        if self.tril_mask:
+            mask_tril = torch.tril(torch.ones_like(span_logits), diagonal=-1)
+            span_logits = span_logits - mask_tril * 1e12
+
+        return span_logits  
---
+++ nlhappy/models/entity_extraction/biaffine.py
@@ -0,0 +1,114 @@
+from ...utils.make_model import PLMBaseModel, align_token_span
+from ...layers.classifier import BiaffineClassifier
+from ...layers.dropout import MultiDropout
+from ...layers.loss import MultiLabelCategoricalCrossEntropy
+from ...metrics.span import SpanF1
+import torch
+
+
+class BiaffineForEntityExtraction(PLMBaseModel):
+    def __init__(self,
+                 lr: float,
+                 hidden_size: int = 128,
+                 threshold: float = 0.0,
+                 scheduler: str = 'linear_warmup_step',
+                 weight_decay: float = 0.01,
+                 **kwargs) -> None:
+        super().__init__()
+        
+        self.plm = self.get_plm_architecture()
+        self.classifier = BiaffineClassifier(input_size=self.plm.config.hidden_size,
+                                             hidden_size=hidden_size,
+                                             output_size=len(self.hparams.label2id))
+        self.dropout = MultiDropout()
+        
+        self.criterion = MultiLabelCategoricalCrossEntropy()
+
+        self.train_metric = SpanF1()
+        self.val_metric = SpanF1()
+        self.test_metric = SpanF1()
+
+
+    def forward(self, input_ids, token_type_ids, attention_mask):
+        x = self.plm(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask).last_hidden_state
+        x = self.dropout(x)
+        x = self.classifier(x, mask=attention_mask)
+        return x
+    
+    
+    def step(self, batch):
+        input_ids = batch['input_ids']
+        token_type_ids = batch['token_type_ids']
+        attention_mask = batch['attention_mask']
+        span_ids = batch['label_ids']
+        logits = self(input_ids, token_type_ids, attention_mask)
+        pred = logits.ge(self.hparams.threshold).float()
+        batch_size, ent_type_size = logits.shape[:2]
+        y_true = span_ids.reshape(batch_size*ent_type_size, -1)
+        y_pred = logits.reshape(batch_size*ent_type_size, -1)
+        loss = self.criterion(y_pred, y_true)
+        return loss, pred, span_ids
+
+        
+    def training_step(self, batch, batch_idx):
+        loss, pred, true = self.step(batch)
+        self.train_metric(pred, true)
+        self.log('train/f1', self.train_metric, on_step=True, on_epoch=True, prog_bar=True)
+        return {'loss':loss}
+    
+    
+    def validation_step(self, batch, batch_idx):
+        loss, pred, true = self.step(batch)
+        self.val_metric(pred, true)
+        self.log('val/f1', self.val_metric, on_step=False, on_epoch=True, prog_bar=True)
+        return {'loss':loss}
+    
+    
+    def test_step(self, batch, batch_idx):
+        loss, pred, true = self.shared_step(batch)
+        self.test_metric(pred, true)
+        self.log('test/f1', self.test_metric, on_step=False, on_epoch=True, prog_bar=True)
+        return {'loss': loss}
+    
+    
+    def configure_optimizers(self):
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        grouped_parameters = [
+            {'params': [p for n, p in self.plm.named_parameters() if not any(nd in n for nd in no_decay)],
+             'lr': self.hparams.lr, 'weight_decay': self.hparams.weight_decay},
+            {'params': [p for n, p in self.plm.named_parameters() if any(nd in n for nd in no_decay)],
+             'lr': self.hparams.lr, 'weight_decay': 0.0},
+            {'params': [p for n, p in self.classifier.named_parameters() if not any(nd in n for nd in no_decay)],
+             'lr': self.hparams.lr * 5, 'weight_decay': self.hparams.weight_decay},
+            {'params': [p for n, p in self.classifier.named_parameters() if any(nd in n for nd in no_decay)],
+             'lr': self.hparams.lr * 5, 'weight_decay': 0.0}
+        ]
+        optimizer = torch.optim.AdamW(grouped_parameters)
+        scheduler_config = self.get_scheduler_config(optimizer, self.hparams.scheduler)
+        return [optimizer], [scheduler_config]
+    
+    
+    def predict(self, text: str, device: str='cpu', threshold = None):
+        if threshold is None:
+            threshold = self.hparams.threshold
+        inputs = self.tokenizer(text,
+                                add_special_tokens=True,
+                                max_length=self.hparams.max_length,
+                                truncation=True,
+                                return_offsets_mapping=True,
+                                return_tensors='pt')
+        mapping = inputs.pop('offset_mapping')
+        mapping = mapping[0].tolist()
+        inputs.to(device)
+        logits = self(**inputs)
+        spans_ls = torch.nonzero(logits>threshold).tolist()
+        spans = []
+        for span in spans_ls :
+            start = span[2]
+            end = span[3]
+            char_span = align_token_span((start, end+1), mapping)
+            start = char_span[0]
+            end = char_span[1]
+            span_text = text[start:end]
+            spans.append([start, end, self.hparams.id2label[span[1]], span_text])
+        return spans
