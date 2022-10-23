e7272f97de1f4f864943138cb21dec12333ca2da
善若水 <790990241@qq.om>
2022-10-23 03:00:10 +0000

    Remove SpanOffsetF1 metric

--- nlhappy/metrics/span.py
+++ nlhappy/metrics/span.py
@@ -23,30 +23,3 @@ class SpanF1(Metric):
 
     def compute(self):
         return 2 * self.correct / (self.all_pred + self.all_true + 1e-5)
-
-
-class SpanOffsetF1(Metric):
-
-    full_state_update: Optional[bool] = False
-
-    def __init__(self):
-        super().__init__()
-        self.add_state('correct', default=torch.tensor(0.0), dist_reduce_fx='sum')
-        self.add_state('all_pred', default=torch.tensor(0.0), dist_reduce_fx='sum')
-        self.add_state('all_true', default=torch.tensor(0.0), dist_reduce_fx='sum')
-
-    def update(self, pred: List[Set[tuple]], true: List[Set[tuple]]) -> None:
-        """
-        参数:
-        - pred: batch_size大小的列表, 列表内为每个样本的预测的span offset的集合(set)
-        - true: 同pred
-        """
-        assert len(pred) == len(true), f'pred : {pred}, true : {true}'
-        for p, t in zip(pred, true):
-            self.correct += len(p & t)
-            self.all_pred += len(p)
-            self.all_true += len(t)
-    
-    def compute(self):
-        # 下面这个公式可以通过化简得到
-        return 2 * self.correct / (self.all_pred + self.all_true + 1e-10)
