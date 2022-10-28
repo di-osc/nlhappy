b6e9227df582aed68cb0c62dbb9b977317a4efc9
wangmengdi <790990241@qq.com>
2022-10-28 17:12:46 +0800

    Add EfficientBiaffine

--- nlhappy/layers/classifier/biaffine.py
+++ nlhappy/layers/classifier/biaffine.py
@@ -96,3 +96,66 @@ class BiaffineSpanClassifier(nn.Module):
             span_logits = span_logits - mask_tril * 1e12
             
         return span_logits.permute(0,2,3,1)
+    
+    
+class EfficientBiaffine(nn.Module):
+    """更加高效的利用biaffine机制做span分类,想法来源于EfficientGlobalPointer
+    
+    - 输入: batch_size, seq_length, hidden_size
+    - 输出: batch_size, seq_length, seq_length, output_size(num_class)
+    """
+    def __init__(self, 
+                 input_size: int, 
+                 hidden_size: int,
+                 output_size: int,
+                 add_rope: bool =True,
+                 tril_mask: bool =True):
+        super().__init__()
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.RoPE = add_rope
+        self.tril_mask = tril_mask
+        self.input_size = input_size
+        self.head = nn.Linear(input_size, hidden_size)
+        self.tail = nn.Linear(input_size, hidden_size)
+        weight = torch.zeros(output_size, hidden_size, hidden_size)
+        nn.init.xavier_normal_(weight)
+        self.cls = nn.Parameter(weight, requires_grad=True)
+
+    def forward(self, inputs, mask=None):
+        qw = self.head(inputs)
+        kw = self.tail(inputs)
+        # RoPE编码
+        if self.RoPE:
+            pos = SinusoidalPositionEmbedding(self.hidden_size, 'zero')(inputs)
+            cos_pos = pos[...,1::2].repeat(1,1,2)
+            sin_pos = pos[...,::2].repeat(1,1,2)
+            qw2 = torch.stack([-qw[..., 1::2], qw[..., ::2]], 3)
+            qw2 = torch.reshape(qw2, qw.shape)
+            qw = qw * cos_pos + qw2 * sin_pos
+            kw2 = torch.stack([-kw[..., 1::2], kw[..., ::2]], 3)
+            kw2 = torch.reshape(kw2, kw.shape)
+            kw = kw * cos_pos + kw2 * sin_pos
+        # 得到每个span的边界分数 -> batch, 1, seqlen, seqlen
+        span = torch.einsum('bmd , bnd -> bmn', qw, kw) / self.hidden_size**0.5
+        span = span.unsqueeze(1)
+        
+        # 得到每个span的类别分数 基于双仿射机制得到类别分数 -> batch, class, seqlen, seqlen
+        cls = torch.einsum('bxi,oij,byj->boxy', qw, self.cls, kw)
+        
+        # 边界得分与类别得分相加 除2为了模型更容易学习
+        logits = (span + cls) / 2
+        
+        # padding mask
+        if mask is not None:
+            batch_size = inputs.size()[0]
+            seq_len = inputs.size()[1]
+            pad_mask = mask.unsqueeze(1).unsqueeze(1).expand(batch_size, self.output_size, seq_len, seq_len)
+            logits = logits*pad_mask - (1-pad_mask)*1e12
+
+        # tril mask
+        if self.tril_mask:
+            mask = torch.tril(torch.ones_like(logits), -1) 
+            logits = logits - mask * 1e12
+
+        return logits
