4a1810dbc8b4a707b1ede84f2420641d04b08d4b
wangmengdi <790990241@qq.com>
2022-11-21 14:33:29 +0800

    Refactor one span classifier

--- nlhappy/layers/classifier/__init__.py
+++ nlhappy/layers/classifier/__init__.py
@@ -2,3 +2,4 @@ from .global_pointer import GlobalPointer, EfficientGlobalPointer
 from .simple_dense import SimpleDense
 from .crf import CRF
 from .biaffine import Biaffine, BiaffineSpanClassifier, EfficientBiaffineSpanClassifier
+from .onerel import OneRelSpanClassifier
--- nlhappy/layers/classifier/onerel.py
+++ nlhappy/layers/classifier/onerel.py
@@ -2,20 +2,25 @@ import torch.nn as nn
 import torch
 
 
-class OneRelClassifier(nn.Module):
+class OneRelSpanClassifier(nn.Module):
+    """枚举出sel_len * sel_len个span,并进行分类
+
+    Args:
+        input_size(int): 输入特征维度
+        output_size(int): 分类的类别数量
+    输出形状: [batch, seq, seq, output_size]
+    """
     
     def __init__(self,
                  input_size: int,
                  output_size: int,
-                 tag_size: int,
-                 dropout_prob: float):
+                 ):
         super().__init__()
         self.output_size = output_size
-        self.tag_size =tag_size
-        self.linear1 = nn.Linear(input_size*2, input_size*3)
-        self.dropout =  nn.Dropout(dropout_prob)
+        self.linear1 = nn.Linear(input_size*2, input_size)
         self.activation = nn.ReLU()
-        self.linear2 = nn.Linear(input_size*3, tag_size*output_size)
+        self.linear2 = nn.Linear(input_size, output_size)
+        self.layernorm = nn.LayerNorm(input_size)
         
     def forward(self, x): 
         batch_size, seq_len, hidden_size = x.size()
@@ -26,7 +31,7 @@ class OneRelClassifier(nn.Module):
         # 两两token的特征拼接在一起
         pairs = torch.cat([head, tail], dim=-1)
         pairs = self.linear1(pairs)
-        pairs = self.dropout(pairs)
+        pairs = self.layernorm(pairs)
         pairs = self.activation(pairs)
-        scores = self.linear2(pairs).reshape(batch_size, seq_len, seq_len, self.output_size, self.tag_size)
+        scores = self.linear2(pairs).reshape(batch_size, seq_len, seq_len, self.output_size)
         return scores
