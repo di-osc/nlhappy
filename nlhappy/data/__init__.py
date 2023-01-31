a199125780df90c92355b7a307935c20cc21a875
wangmengdi <790990241@qq.com>
2023-01-31 09:07:54 +0800

    Add couplet

--- nlhappy/data/__init__.py
+++ nlhappy/data/__init__.py
@@ -1,6 +1,7 @@
 from .doc import Doc, DocBin, Entity, Relation, Event
 from .dataset import Dataset, DatasetDict
+from .couplet import Couplet, CoupletBin
 
 
 
-__all__ = ["Doc", "DocBin", "Entity", "Relation", "Event", "Dataset", "DatasetDict"]
+__all__ = ["Doc", "DocBin", "Entity", "Relation", "Event", "Dataset", "DatasetDict", "Couplet", "CoupletBin"]
